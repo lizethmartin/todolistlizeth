f618d21c631cc740a4512e3c2cd44df3087decf6
LizethMartin <lizethalejandramartinezcamargo@gmail.com>
2026-04-26 16:16:06 -0500

    fix ios deployment target

--- platforms/ios/www/chunk-EWZ55BBR.js
+++
@@ -1,1 +0,0 @@
-import{a,b}from"./chunk-OSB7IZV2.js";import"./chunk-7IDS6YIB.js";import"./chunk-BJ43HUSB.js";import"./chunk-RZX54AM5.js";import"./chunk-NMFL75IO.js";import"./chunk-TSRGIXR5.js";export{b as iosTransitionAnimation,a as shadow};
